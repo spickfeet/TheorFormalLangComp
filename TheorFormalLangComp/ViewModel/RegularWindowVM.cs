41110f2eb12d7b3a007ebd2838c672ae0a237ffc
DaryaBorovinskaya <dashhaha17@gmail.com>
2025-03-20 14:19:23 +0700

    regular window

---
+++ TheorFormalLangComp/ViewModel/RegularWindowVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheorFormalLangComp.Model; 
+
+namespace TheorFormalLangComp.ViewModel
+{
+    public class RegularWindowVM : BasicVM
+    {
+
+        public string RegularText { get; set; }
+        
+
+        public RegularWindowVM(int menuNumb)
+        {
+            RegularTextClass reg = new(); 
+
+            switch(menuNumb)
+            {
+                case 1:
+                    RegularText = reg.Formule;
+                    break;
+                case 2:
+                    RegularText = reg.Language;
+                    break;
+                case 3:
+                    RegularText = reg.Example;
+                    break;
+            }
+        }
+
+    }
+}
